e2f721e6f07581564eb54a99af5a6059722f4a60
DanLevyM <dan.levy.pro@gmail.com>
2022-04-17 20:39:56 +0200

    slider final

--- 1.Slider/src/Components/Slider/Slider.js
+++ 1.Slider/src/Components/Slider/Slider.js
@@ -10,20 +10,43 @@ export default function Slider() {
     inProgress: false,
   });
 
-  const prevSlide = () => {
-    if (slideAnim.index !== 1) {
-      setSlideAnim({index: slideAnim.index - 1, inProgress: true})
-    } else if (slideAnim.index === 1) {
-      setSlideAnim({index: dataSlider.length, inProgress: true})
+  const nextSlide = () => {
+    if (slideAnim.index !== dataSlider.length && !slideAnim.inProgress) {
+      setSlideAnim({index: slideAnim.index + 1, inProgress: true})
+      setTimeout(() => {
+        setSlideAnim({index: slideAnim.index + 1, inProgress: false})
+      }, 400)
+
+    } else if (slideAnim.index === dataSlider.length && !slideAnim.inProgress) {
+      setSlideAnim({index: 1 , inProgress: true})
+      setTimeout(() => {
+        setSlideAnim({index: 1, inProgress: false})
+      }, 400)
     }
   }
 
-  const nextSlide = () => {
-    if (slideAnim.index !== dataSlider.length) {
-      setSlideAnim({index: slideAnim.index+1, inProgress: true})
-    } else if (slideAnim.index === dataSlider.length) {
-      setSlideAnim({index: 1 , inProgress: true})
+  const prevSlide = () => {
+    if (slideAnim.index !== 1 && !slideAnim.inProgress && !slideAnim.inProgress) {
+
+      setSlideAnim({index: slideAnim.index - 1, inProgress: true})
+      setTimeout(() => {
+        setSlideAnim({index: slideAnim.index - 1, inProgress: false})
+      }, 400)
+
+    } else if (slideAnim.index === 1 && !slideAnim.inProgress) {
+      setSlideAnim({index: dataSlider.length, inProgress: true})
+      setTimeout(() => {
+        setSlideAnim({index: dataSlider.length, inProgress: false})
+      }, 400)
     }
+  }
+
+  const moveDot = index => {
+    setSlideAnim({index: index, inProgress: true})
+    setTimeout(() => {
+      setSlideAnim({index: index, inProgress: false})
+    }, 400)
+
   }
 
   return (
@@ -34,6 +57,16 @@ export default function Slider() {
             <img src={`/Imgs/img${index+1}.jpg`} alt=""/>
             <BtnSlider moveSlide={prevSlide} direction={'prev'}/>
             <BtnSlider moveSlide={nextSlide} direction={'next'}/>
+
+            <div className="container-dots">
+              {dataSlider.map((obj, indexForDots) => {
+                return (
+                  <div key={obj.id} className={slideAnim.index === indexForDots + 1 ? 'dot active' : 'dot'}
+                  onClick={() => moveDot(indexForDots + 1)}
+                  />
+                )
+              })}
+            </div>
           </div>
         )
       })}
