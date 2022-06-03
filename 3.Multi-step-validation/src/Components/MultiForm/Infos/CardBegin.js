3920d0b1068eb667fd7f853dd62065bdfc09997a
DanLevyM <dan.levy.pro@gmail.com>
2022-06-03 22:31:13 +0200

    reprise

---
+++ 3.Multi-step-validation/src/Components/MultiForm/Infos/CardBegin.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Card.css';
+
+export default function CardBegin () {
+  return (
+    <div className='card'>
+      <h1>🍣 Aide-nous à ravir tes papilles 🐡</h1>
+      <button>Commencer</button>
+    </div>
+  );
+}
+
---
+++ 3.Multi-step-validation/src/Components/MultiForm/Infos/CardEnd.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Card.css';
+
+export default function CardEnd () {
+  return (
+    <div className='card'>
+      <h1>Bravo !</h1>
+      <p>Bla bla bla</p>
+    </div>
+  );
+}
+
